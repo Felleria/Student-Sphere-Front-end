b5a8142ac7f2a2dd0a2755ac517a3997d2f21f82
molly <mollynzembi@gmail.com>
2024-08-12 16:20:17 +0300

    dashboard routes

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,28 +1,3 @@
-/*
-import Sidebar from '../components/Sidebar';
-import { Outlet } from 'react-router-dom';
-
-const studentLinks = [
-  { name: 'Profile', path: 'profile' },
-  { name: 'Progress', path: 'progress' },
-  { name: 'Attendance', path: 'attendance' },
-  { name: 'Grades', path: 'grades' },
-  { name: 'Assigned Teachers', path: 'assigned-teachers' },
-];
-
-const StudentDashboard = () => {
-  return (
-    <div className="flex">
-      <Sidebar links={studentLinks} />
-      <div className="flex-1 p-6 bg-gray-100">
-        <Outlet />
-      </div>
-    </div>
-  );
-};
-
-export default StudentDashboard;
-*/
 import { Link, Routes, Route } from 'react-router-dom';
 import Profile from '../components/student/Profile';
 import Grades from '../components/student/Grades';
